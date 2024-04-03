a29f28a2d814a196248970125564431c32eb39b7
João Victor <larger13@hotmail.com>
2024-04-03 18:44:36 -0300

    Criação do Projeto
    Trabalho template

---
+++ Uniftec.ProjetoSistemasWeb/SocialUniftec/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "Uniftec.ProjetoSistemasWeb.SocialUniftec.Session";
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
