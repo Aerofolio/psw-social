a1aea47dcb1f238038d398e13b542882ecfb2e23
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2024-06-18 21:50:11 -0300

    Criação do usuário através do serviço

--- Uniftec.ProjetoSistemasWeb/SocialUniftec/Controllers/UsuarioController.cs
+++ Uniftec.ProjetoSistemasWeb/SocialUniftec/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SocialUniftec.Filtres;
 using SocialUniftec.Models;
+using SocialUniftec.Website.Backend.HTTPClient;
 
 namespace SocialUniftec.Controllers
 {
@@ -22,11 +23,11 @@ namespace SocialUniftec.Controllers
             if (ModelState.IsValid)
             {
                 return Redirect("Postagem/Feed");
-			} 
+            } 
             else
             {
-				return this.Index();
-			}
+                return this.Index();
+            }
 
         }
 
@@ -36,23 +37,51 @@ namespace SocialUniftec.Controllers
         }
 
         [HttpPost]
-		public IActionResult Cadastrar(UsuarioCadastroModel usuarioCadastro)
-		{
-
+        public IActionResult Cadastrar(UsuarioCadastroModel usuarioCadastro)
+        {
+            
             if (usuarioCadastro.Senha != null && usuarioCadastro.SenhaRepetida != null && !usuarioCadastro.Senha.Equals(usuarioCadastro.SenhaRepetida))
             {
                 ModelState.AddModelError("senhainvalida", "As senhas não coincidem!");
             }
+            
 
-			if (ModelState.IsValid)
-			{
-				return Redirect("Postagem/Feed");
-			}
-			else
-			{
-				return View("Cadastrar");
-			}
-		}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var a = new Website.Backend.UsuarioModel()
+                    {
+                        Nome = usuarioCadastro.Nome,
+                        Sobrenome = usuarioCadastro.Sobrenome,
+                        Email = usuarioCadastro.Email,
+                        Senha = usuarioCadastro.Senha,
+                        DataComemorativa = usuarioCadastro.DataComemorativa,
+                        Sexo = Website.Backend.TipoSexoModel.Masculino,
+                        Bio = "teste",
+                        FotoPerfil = string.Empty,
+                        Cidade = "teste",
+                        Uf = Website.Backend.EstadosBrasilModel.AC,
+                        Telefone = "teste",
+                        Documento = "teste",
+                        Tipo = Website.Backend.TipoPessoaModel.Juridica,
+                        Amigos = []
+                    };
+                    
+                    var request = new APIHttpClient("http://localhost:5048/api/").Post("Usuario", a);
+                    
+                    return Redirect("Postagem/Feed");
+                }
+                catch (Exception ex)
+                {
+                    return View("Error");   
+                }
+            }
+            else
+            {
+                return View("Cadastrar");
+            }
+        }
 
         public IActionResult Perfil(int idUsuario)
         {
