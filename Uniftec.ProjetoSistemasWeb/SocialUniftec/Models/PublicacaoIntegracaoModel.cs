2f1623a09d84d06c02d45937ba227fc96c664bdd
João Victor <larger13@hotmail.com>
2024-06-27 19:43:29 -0300

    Limpando o controller para implementar o feed

---
+++ Uniftec.ProjetoSistemasWeb/SocialUniftec/Models/PublicacaoIntegracaoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SocialUniftec.Website.Models
+{
+    public class PublicacaoIntegracaoModel
+    {
+        public Guid Id { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Descricao { get; set; }
+
+        public DateTime DataPublicacao { get; set; }
+
+        public List<PublicacaoMidiaIntegracao> Midias { get; set; }
+
+    }
+}
---
+++ Uniftec.ProjetoSistemasWeb/SocialUniftec/Models/PublicacaoMidiaIntegracao.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialUniftec.Website.Models
+{
+    public class PublicacaoMidiaIntegracao
+    {
+        public string FileContents { get; set; }
+        public string ContentType { get; set; }
+        public string FileDownloadName { get; set; }
+
+    }
+}
